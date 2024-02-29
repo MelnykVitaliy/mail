b1987dcf70fa478421eab3ce4751a2cb17b48114
Vitaliy Melnyk <71925022+MelnykVitaliy@users.noreply.github.com>
2024-02-29 17:49:04 +0200

    Add files via upload

---
+++ config.sample.php
@@ -0,0 +1,13 @@
+<?php
+define('SMTP_HOST', 'YOUR_HOST_HERE');
+define('SMTP_USERNAME', 'YOUR_EMAIL_HERE');
+define('SMTP_PASSWORD', 'YOUR_PASSWORD_HERE');
+define('SMTP_SECURE','YOUR_SECURE_HERE');
+define('SMTP_PORT', 'YOUR_PORT_HERE');
+
+$sites = [
+    'https://www.imdb.com/',
+    'https://opt.demo.magefan.top',
+    'https://nordicpstheme.nordicoutdoor.co.uk/'
+];
+?>
